76b199c959b3a8d9630f42677c0b119aeb357c24
jonay2000 <jonabent@gmail.com>
2023-04-09 12:15:37 +0200

    add fili key

--- users/jonathan.nix
+++ users/jonathan.nix
@@ -12,6 +12,8 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIET69oniNUA2nJV5+GxQ6XuK+vQbO8Uhtgrp1TrmiXVi jonathan@ori"
       # bastion
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHJT6QJcxhUKjvHBv3Bd1rugyfAFUpxIe9cu1Frw3ylL jonathan@bastion"
+      # fili
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC0pmCsQeMMJ0r3o/XN7Zw8YFa9OEqrL3ikoGTK0OUY6 jonathan@fili"
     ];
 
     # Make me admin
